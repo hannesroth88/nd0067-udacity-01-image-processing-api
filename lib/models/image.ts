5c7f911fac69ec78eb9ba3fd56d85119fc0255b8
Roth Hannes (GS/PJ-DIA1) <Hannes.Roth2@de.bosch.com>
2022-01-03 21:59:36 +0100

    added a route

---
+++ lib/models/image.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface Image {
+    width: string;
+    height: string;
+  }
+
+export {Image}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express = require('express');
+import {Image} from '../lib/models/image'
+
 const app = express();
 
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+
+// define a route handler for the default home page
+app.get( "/api/images", ( req, res ) => {
+    console.log("JSON.stringify(req)")
+    console.log(JSON.stringify(req.query))
     res.send( "Hello world!" );
 } );
 
